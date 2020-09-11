dea232b10ca307a5cd39b85d99e409eed8f5c516
Lawrence-Leong <leong.l@wehi.edu.au>
2020-09-11 16:53:02 +1000

    removed (cofactor=5) from arcsinh option in transformation selection

--- inst/shinyGUI/ui.R
+++ inst/shinyGUI/ui.R
@@ -78,10 +78,17 @@ markerDistribution <-fluidPage(
     ),
     column(3,
            selectInput("choiceTransformation", "Select the Transformation Applied:",
+<<<<<<< HEAD
                        list("ArcSinh (Cofactor = 5)" = "AC5",
                             "ArcSinh (Cofactor = 0.01)" = "AC001",
                             "Log" = "log",
                             "Linear" = "lin")),
+=======
+                       list("ArcSinh" = "default",
+                            #"ArcSinh (Cofactor = 0.01)" = "AC001",
+                            #"Linear" = "lin",
+                            "Log" = "log")),
+>>>>>>> removed (cofactor=5) from arcsinh option in transformation selection
     ),
     column(3,
            h5(strong("Select the file type and Download Plot:")),
